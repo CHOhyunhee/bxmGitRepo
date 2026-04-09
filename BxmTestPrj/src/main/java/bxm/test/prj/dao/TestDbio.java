d8e572c8dd75be53beb7e842fc3284c523f06308
CHOhyunhee <hyunhee425@naver.com>
2026-04-09 15:37:07 +0900

    first commit

---
+++ BxmTestPrj/src/main/java/bxm/test/prj/dao/TestDbio.java
@@ -0,0 +1,33 @@
+/**
+ * Generated 2026. 04. 09. 15:17:26
+ */
+package bxm.test.prj.dao;
+
+import bxm.common.annotaion.BxmCategory;
+import bxm.container.annotation.BxmDataAccess;
+import bxm.container.annotation.TestValue;
+import bxm.container.annotation.TestValues;
+import bxm.test.prj.dto.TestDto;
+import org.apache.ibatis.annotations.Param;
+
+/**
+ * 
+ *
+ * @author sysadmin
+ */
+@SuppressWarnings({ "all" })
+@BxmDataAccess(mapper = "bxm/test/prj/dao/TestDbio.dbio", datasource = "MainDS")
+@BxmCategory(logicalName="임직원정보조회 DBIO", description="", author="sysadmin")
+public interface TestDbio
+{
+	/**
+	 * 
+	 */
+	@TestValues({
+		@TestValue(values="feduEmpNo=7369")
+	})
+	@BxmCategory(logicalName="", description="", author="sysadmin")
+	TestDto select001(
+		@Param("feduEmpNo")String feduEmpNo
+		);
+}
---
+++ BxmTestPrj/src/main/java/bxm/test/prj/service/TestService.java
@@ -0,0 +1,97 @@
+package bxm.test.prj.service;
+
+import bxm.common.annotaion.BxmCategory;
+import bxm.container.annotation.BxmService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import bxm.container.annotation.BxmServiceOperation;
+import bxm.dft.app.DefaultApplicationException;
+import bxm.dft.context.DefaultApplicationContext;
+import bxm.test.prj.bean.TestBean;
+import bxm.test.prj.dto.TestDto;
+import bxm.test.prj.dto.TestInDto;
+
+/**
+ * 
+ *
+ * @author sysadmin
+ */
+@BxmService("TestService")
+@BxmCategory(logicalName="", author="sysadmin")
+public class TestService {
+	private Logger logger= LoggerFactory.getLogger(getClass());
+	
+	/**
+	 * 
+	 */
+	private TestBean bean;
+	
+	/**
+	 * 
+	 * 
+	 * @param input 
+	 * @return 
+	 */
+	@BxmServiceOperation("getEmpInf")
+	@BxmCategory(logicalName="단건조회 메소드", description="", author="sysadmin")
+	public TestDto getEmpInf(TestInDto input)
+	{
+		logger.debug("============== SERVICE START ==============");
+		logger.debug("input= {}", input);
+		
+		/**
+		* @BXMType GetBean
+		*/
+		bean= DefaultApplicationContext.getBean(bean, TestBean.class);
+		/**
+		 * @BXMType IF
+		 * @Desc 입력 검증 : 필수 입력 값 검증 코드를 작성 하십시오.
+		 */
+		if ( input== null ) /**null check*/{
+			// TODO 적합한 메시지 코드로 변경 하십시오.
+			throw new DefaultApplicationException("BXME00001", new Object[]{""}, new Object[]{input.getFeduEmpNo()});
+		}
+		else if ( input.getFeduEmpNo()== null ) /**null check - */{
+			// TODO 적합한 메시지 코드로 변경 하십시오.
+			throw new DefaultApplicationException("msg_code", new Object[]{""});
+		}
+		/**
+		 * @BXMType VariableDeclaration
+		 */
+		TestInDto beanInput= new TestInDto();
+		beanInput.setFeduEmpNo(input.getFeduEmpNo()); //  FW샘플 임직원번호 
+		/**
+		 * @BXMType VariableDeclaration
+		 */
+		TestDto out= new TestDto();
+		/**
+		 * @BXMType BeanCall
+		 * @Desc 
+		 */
+		TestDto beanOutput= bean.getEmp(beanInput);
+		/**
+		 * @BXMType IF
+		 * @Desc 입력 검증 : 필수 입력 값 검증 코드를 작성 하십시오.
+		 */
+		if ( beanOutput== null ) /**null check*/{
+			// TODO 적합한 메시지 코드로 변경 하십시오.
+			throw new DefaultApplicationException("msg_code", new Object[]{""});
+		}
+		out.setFeduEmpNo(beanOutput.getFeduEmpNo()); //  FW샘플 임직원번호 
+		out.setFeduEmpNm(beanOutput.getFeduEmpNm()); //  FW샘플 임직원명 
+		out.setFeduOccpNm(beanOutput.getFeduOccpNm()); //  FW샘플 직업명 
+		out.setFeduMngrEmpNo(beanOutput.getFeduMngrEmpNo()); //  FW샘플 관리자임직원번호 
+		out.setFeduHireDt(beanOutput.getFeduHireDt()); //  FW샘플 입사일자 
+		out.setFeduPayAmt(beanOutput.getFeduPayAmt()); //  FW샘플 급여금액 
+		out.setFeduDeptNo(beanOutput.getFeduDeptNo()); //  FW샘플 부서번호 
+		
+		logger.debug("out= {}", out);
+		logger.debug("============== SERVICE END ==============");
+		
+		//TODO :
+		
+		return out;
+		
+	} 
+	
+}
